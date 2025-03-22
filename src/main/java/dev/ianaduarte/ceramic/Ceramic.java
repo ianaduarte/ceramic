0b8dcea9d78c0cb63b05648dee88910d8849abf7
ianaduarte <118121781+ianaduarte@users.noreply.github.com>
2025-03-22 12:49:19 -0400

    Actually load/reload models

--- src/main/java/dev/ianaduarte/ceramic/Ceramic.java
+++ src/main/java/dev/ianaduarte/ceramic/Ceramic.java
@@ -1,12 +1,16 @@
 package dev.ianaduarte.ceramic;
 
+import dev.ianaduarte.ceramic.layers.CeramicModelLayers;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.server.packs.PackType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.resources.ResourceLocation;
 
-public class Ceramic implements ModInitializer {
+public class Ceramic implements ClientModInitializer {
 	public static final String MOD_ID = "ceramic";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	
@@ -14,7 +18,9 @@ public class Ceramic implements ModInitializer {
 		return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
 	}
 	
+	
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
+		ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new CeramicModelLayers());
 	}
 }
--- src/main/java/dev/ianaduarte/ceramic/layers/CeramicModelLayers.java
+++ src/main/java/dev/ianaduarte/ceramic/layers/CeramicModelLayers.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableMap;
 import dev.ianaduarte.ceramic.Ceramic;
 import dev.ianaduarte.ceramic.geom.CeramicModel;
 import dev.ianaduarte.ceramic.geom.definitions.CeramicModelDefinition;
+import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
@@ -12,7 +13,7 @@ import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CeramicModelLayers implements ResourceManagerReloadListener {
+public class CeramicModelLayers implements ResourceManagerReloadListener, IdentifiableResourceReloadListener {
 	private static final Map<ModelLayerLocation, ModelProvider> ALL_PROVIDERS = new HashMap<>();
 	private static Map<ModelLayerLocation, CeramicModelDefinition> MODEL_DEFINITIONS = Map.of();
 	
@@ -42,6 +43,12 @@ public class CeramicModelLayers implements ResourceManagerReloadListener {
 		MODEL_DEFINITIONS = builder.build();
 		Ceramic.LOGGER.info("Baked models layers");
 	}
+	
+	@Override
+	public ResourceLocation getFabricId() {
+		return Ceramic.getLocation("model_layers");
+	}
+	
 	public interface ModelProvider {
 		CeramicModelDefinition get();
 	}
